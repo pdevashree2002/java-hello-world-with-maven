789d7b10ff520557b3ba97105633ad72d1e605e3
pdevashree2002 <149948235+pdevashree2002@users.noreply.github.com>
2024-08-21 16:03:38 +0530

    Update HelloWorld.java wrongly to send email notifications

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,9 +2,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.primt("The current local time is: " + currentTime);
+		  System.out.print("The current local time is: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
-    }
-}
+    
